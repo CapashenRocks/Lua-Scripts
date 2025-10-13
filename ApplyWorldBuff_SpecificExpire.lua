f1e3aa20bb0870d8883e15ca22532fcd0f7e2b0c
CapashenRocks <88005278+CapashenRocks@users.noreply.github.com>
2025-10-13 09:36:46 -0400

    Initial upload

---
+++ ApplyWorldBuff_SpecificExpire.lua
@@ -0,0 +1,59 @@
+-- #####################################################
+-- JerCore's World Buff: Applied only during specific time period
+-- Fall Celebration
+-- Applies spell 957353 at login until 10/31/2025
+-- 957353 is a custom spell, change to your spell!
+-- #####################################################
+
+local SPELL_ID = 957353
+local MESSAGE = "|cffffa500Fall Celebration is now active!|r"
+
+-- Expiration timestamp
+local EXPIRATION = os.time({
+    year = 2025,
+    month = 11,
+    day = 1,
+    hour = 0,
+    min = 1,
+    sec = 0
+})
+
+local removalDone = true
+
+local function OnLogin(event, player)
+    local now = os.time()
+
+    if now < EXPIRATION then
+        player:AddAura(SPELL_ID, player)
+
+        local guid = player:GetGUIDLow()
+
+        CreateLuaEvent(function()
+            local p = GetPlayerByGUID(guid)
+            if p and p:IsInWorld() then
+                p:SendBroadcastMessage(MESSAGE)
+            end
+        end, 3000, 1)
+
+    else
+       
+        if not removalDone and player:HasAura(SPELL_ID) then
+            player:RemoveAura(SPELL_ID)
+        end
+    end
+end
+
+local function CheckExpiration()
+    if not removalDone and os.time() >= EXPIRATION then
+        for _, player in pairs(GetPlayersInWorld()) do
+            if player:HasAura(SPELL_ID) then
+                player:RemoveAura(SPELL_ID)
+            end
+        end
+        removalDone = true
+        SendWorldMessage("Fall Celebration buff has ended.")
+    end
+end
+
+RegisterPlayerEvent(3, OnLogin)            -- 3 = OnLogin
+--CreateLuaEvent(CheckExpiration, 60000, 0)  -- check every 60 seconds
