db83d90081aa16df68a90e20601ea1348bb80764
CapashenRocks <88005278+CapashenRocks@users.noreply.github.com>
2025-10-13 09:20:03 -0400

    Added details and comments
    Removed a spell from the initial learning list for new players.

--- NewPlayerStartSpells.lua
+++ NewPlayerStartSpells.lua
@@ -3,8 +3,9 @@
 -- Description: When a new character enters the world for the first time, they will automatically learn spells identified below.
 -- A one-time broadcast message will also be sent if custom message is setup/defined.
 
-local SPELLS_TO_LEARN = {455532, 20608, 33389, 13864}
--- Spell List: Coreforged skeleton key spell, Reincarnation, Appretice Riding, PW: Fortitude
+-- Add or remove spells to learn that spell on first character login
+local SPELLS_TO_LEARN = {455532, 20608, 33389}
+-- Spell List: Coreforged skeleton key spell, Reincarnation, Appretice Riding
 
 local function OnFirstLogin(event, player)
     for _, spellId in ipairs(SPELLS_TO_LEARN) do
@@ -13,9 +14,10 @@ local function OnFirstLogin(event, player)
         end
     end
 
-    -- Custom message 
+    -- Custom message, change to anything you would like or delete for no messaging
     player:SendBroadcastMessage("You have been granted additional abilities!")
 end
 
 RegisterPlayerEvent(30, OnFirstLogin)
+
 
